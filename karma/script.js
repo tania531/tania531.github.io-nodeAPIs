110eeb35601131831d838740aa18f1be0062ca65
AgileLabs <samer@agilelabs.com>
2015-06-10 15:04:19 -0700

    Real data

--- karma/script.js
+++ karma/script.js
@@ -4,21 +4,20 @@ var leaderboard = function(data) {
 
 $(document).ready(function() {
   $.ajax("data.json").success(function(response) {
-    console.log(response);
+    var sorted = leaderboard(response.data);
+    var $body = $("body");
+    var divArray = [];
+    for (var i = 0; i < sorted.length; i++) {
+      var $div = $("<div>");
+      var $p = $("<p>");
+      $p.text(sorted[i].name + ' ' + sorted[i].points);
+      $div.append($p);
+      divArray.push($div);
+    }
+    $body.append(divArray);
   }).error(function(x, text, error) {
     console.error(x, text, error);
   });
-  var sorted = leaderboard(data);
-  var $body = $("body");
-  var divArray = [];
-  for (var i = 0; i < sorted.length; i++) {
-    var $div = $("<div>");
-    var $p = $("<p>");
-    $p.text(sorted[i].name + ' ' + sorted[i].points);
-    $div.append($p);
-    divArray.push($div);
-  }
-  $body.append(divArray);
 })
 
 function compare(a, b){
