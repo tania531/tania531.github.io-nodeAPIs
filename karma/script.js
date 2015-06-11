d77b70cf3b7992edca25e179904e4e9f6571f67d
Tania <tania.dev77@gmail.com>
2015-06-10 22:52:00 -0700

    karma points app

--- karma/script.js
+++ karma/script.js
@@ -16,45 +16,49 @@ var $$ = {
     this.data[indexInArray].points = newPoints;
     this.storeData();
   },
-  data: []
-}
+  data: [],
+  forceGet : true
+};
 
 $(document).ready(function() {
   $$.readData();
   var sorted = $$.leaderboard();
 
-  var $template = $(".person:first"), $clonedLi;
-  var ppl = sorted.map(function(p, i) {
-    $clonedLi = $template.clone().show();
-    $clonedLi.data("order", i);
-    $clonedLi.find(".name").text(p.name);
-    $clonedLi.find(".points").text(p.points);
-    $clonedLi.find("input").val(p.points);
-    return $clonedLi;
-  });
+  console.log("sorted: ", sorted);
+
+    var $template = $(".person:first"), $clonedLi;
+    var ppl = sorted.map(function(p, i) {
+      $clonedLi = $template.clone().show();
+      $clonedLi.data("order", i);
+      $clonedLi.find(".name").text(p.name);
+      $clonedLi.find(".points").text(p.points);
+      $clonedLi.find("input").val(p.points);
+      return $clonedLi;
+    });
 
   $("#ppl").append(ppl);
 
   $("#ppl")
   .on("dblclick", ".points", function() {
     var person = $(this).parents(".person");
-    console.log(person.data("order"));
+    console.log($(this));
+
+    $(this).closest(".points").hide();
+    $(this).closest(".person").find(".input").removeClass("hidden");
+    $(this).closest("input").show();
+
   })
   .on("keyup", "input", function(event) {
     if (event.which === 13) {
-      console.log("Enter")
+      console.log($(this).val());
+      $(this).closest(".person").find(".points").html($(this).val());
+      $(this).closest(".person").find(".points").show();
+      $(this).closest(".person").find(".input").addClass("hidden");
+      var person = $(this).parents(".person");
+      var index = person.data("order");
+      $$.modifyPointsFor(index, $(this).val());
+      location.reload($$.forceGet);
     }
   });
 
-})
-
-
-
-
-
-
-
-
-
-
-
+});
