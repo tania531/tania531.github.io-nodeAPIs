0442ded2cb495da9527decb3ca4933da9af0a147
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-06-12 13:28:24 -0700

    merge?

--- karma/script.js
+++ karma/script.js
@@ -16,72 +16,54 @@ var $$ = {
     this.data[indexInArray].points = newPoints;
     this.storeData();
   },
-  data: [],
-<<<<<<< HEAD
-  forceGet: true
+  data: []
 }
-=======
-  forceGet : true
-};
->>>>>>> 779d27a7e88a9860a1009968c5da0856ce654152
-
 
 $(document).ready(function() {
   $$.readData();
   var sorted = $$.leaderboard();
-
   console.log("sorted: ", sorted);
 
-    var $template = $(".person:first"), $clonedLi;
-    var ppl = sorted.map(function(p, i) {
-      $clonedLi = $template.clone().show();
-      $clonedLi.data("order", i);
-      $clonedLi.find(".name").text(p.name);
-      $clonedLi.find(".points").text(p.points);
-      $clonedLi.find("input").val(p.points);
-      return $clonedLi;
-    });
-
-  $("#ppl").append(ppl);
-
-  $("#ppl")
-  .on("dblclick", ".points", function() {
-<<<<<<< HEAD
-    $(this).css("display", "none");
-    $(this).next().show();
-  })
-  .on("keyup", "input", function(event) {
-    if (event.which === 13) {
-      var person = $(this).parents(".person");
-      var personIndex = person.data("order");
-      var newVal = $(this).val();
-      $$.modifyPointsFor(personIndex, newVal);
-      $$.storeData($$.data);
-      location.reload($$.forceGet);
-      $(this).hide();
-      $(".points").show();
-=======
-    var person = $(this).parents(".person");
-    console.log($(this));
-
-    $(this).closest(".person").find(".points").hide();
-    $(this).closest(".person").find(".input").removeClass("hidden");
-    $(this).closest("input").show();
-
-  })
-  .on("keyup", "input", function(event) {
-    if (event.which === 13) {
-      console.log($(this).val());
-      $(this).closest(".person").find(".points").html($(this).val());
-      $(this).closest(".person").find(".points").show();
-      $(this).closest(".person").find(".input").addClass("hidden");
-      var person = $(this).parents(".person");
-      var index = person.data("order");
-      console.log("person: ", person);
-      $$.modifyPointsFor(index, $(this).val());
-      location.reload($$.forceGet);
->>>>>>> 779d27a7e88a9860a1009968c5da0856ce654152
-    }
+  // var ppl = [];
+  var $template = $(".person:first"), $clonedLi;
+  // var $template = $("ul li:first"), $clonedLi;
+  var ppl = sorted.map(function(p,i){
+    $clonedLi = $template.clone().show();
+   $clonedLi.find(".name").text(p.name);
+   $clonedLi.find(".points").text(p.points);
+   return $clonedLi;
   });
 
-});
+  // console.log("ppl: ", ppl);
+
+  // var oneOfYou = sorted[5];
+  // var $oneOfYou = $("ul li:first").clone().show();
+  //
+  //  $oneOfYou.find(".name").text(oneOfYou.name);
+  //  $oneOfYou.find(".points").text(oneOfYou.points);
+
+  //  ppl.push($oneOfYou);
+   $("#ppl").append($oneOfYou);
+
+   var oneOfYou = sorted[9];
+   var $oneOfYou = $("ul li:first").clone().show();
+
+   $oneOfYou.find(".name").text(oneOfYou.name);
+   $oneOfYou.find(".points").text(oneOfYou.points);
+   ppl.push($oneOfYou);
+  // $("ul").append($oneOfYou);
+
+  $("ul").append(ppl);
+
+
+  // var $body = $("body");
+  // var divArray = [];
+  // for (var i = 0; i < sorted.length; i++) {
+  //   var $div = $("<div>");
+  //   var $p = $("<p>");
+  //   $p.text(sorted[i].name + ' ' + sorted[i].points);
+  //   $div.append($p);
+  //   divArray.push($div);
+  // }
+  // $body.append(divArray);
+})
